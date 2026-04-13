a647c777c0291f52402fbe4d8ad768ffdd7b8857
pwqw <>
2026-04-13 11:43:51 +0000

    fix(screenshots): agregar import flutter/services.dart para SystemChrome

--- integration_test/screenshot_journey_test.dart
+++ integration_test/screenshot_journey_test.dart
@@ -6,6 +6,7 @@ import 'package:app/ui/screens/main.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:provider/provider.dart';
